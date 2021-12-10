53523dd6677950ceebd9ac5514b3c8c2b1a7a4b3
Silke-Dona <92987924+Silke-Dona@users.noreply.github.com>
2021-12-10 14:05:08 +0100

    Fixed
    Scaled cockpit
    Fixed turbulences issue
    
    Done animation

--- helper.py
+++ helper.py
@@ -94,18 +94,24 @@ def create_line():
     x, y, z, I, u, delta_u, U_prime = data
     return mlab.plot3d(x, y, z, I, tube_radius=9, tube_sides=30, opacity=0.45), y, I
 
+# def create_line():
+#     data = np.genfromtxt("data/csv/data.csv", delimiter=";")
+#     data = np.transpose(data)
+#     x, y, z, I, u, delta_u, U_prime = data
+#     return mlab.plot3d(x, y, z, delta_u, tube_radius=9, tube_sides=30, opacity=0.45), y, delta_u
+
 
 def create_cockpit():
     return mlab.triangular_mesh(cockpit_base_v_x, cockpit_base_v_y, cockpit_base_v_z, cockpit_base_f,
-                                color=(1, 1, 1)), \
+                                color=(0.86098, 0.90196, 0.9647)), \
            mlab.triangular_mesh(cockpit0_v_x, cockpit0_v_y, cockpit0_v_z, cockpit0_f,
-                                color=(0, 0, 0)), \
-           mlab.triangular_mesh(cockpit1_v_x, cockpit1_v_y, cockpit1_v_z, cockpit1_f, color=(1, 1, 1)), \
-           mlab.triangular_mesh(cockpit2_v_x, cockpit2_v_y, cockpit2_v_z, cockpit2_f, color=(0, 0, 0)), \
-           mlab.triangular_mesh(cockpit3_v_x, cockpit3_v_y, cockpit3_v_z, cockpit3_f, color=(1, 1, 1)), \
-           mlab.triangular_mesh(cockpit4_v_x, cockpit4_v_y, cockpit4_v_z, cockpit4_f, color=(0, 0, 0)), \
-           mlab.triangular_mesh(cockpit5_v_x, cockpit5_v_y, cockpit5_v_z, cockpit5_f, color=(1, 1, 1)), \
-           mlab.triangular_mesh(cockpit6_v_x, cockpit6_v_y, cockpit6_v_z, cockpit6_f, color=(0, 0, 0))
+                                color=(0.4, 0.4549, 0.5333)), \
+           mlab.triangular_mesh(cockpit1_v_x, cockpit1_v_y, cockpit1_v_z, cockpit1_f, color=(0.4666, 0.5176, 0.5647)), \
+           mlab.triangular_mesh(cockpit2_v_x, cockpit2_v_y, cockpit2_v_z, cockpit2_f, color=(0.6157, 0.6118, 0.6275)), \
+           mlab.triangular_mesh(cockpit3_v_x, cockpit3_v_y, cockpit3_v_z, cockpit3_f, color=(0.3255, 0.3451, 0.3725)), \
+           mlab.triangular_mesh(cockpit4_v_x, cockpit4_v_y, cockpit4_v_z, cockpit4_f, color=(0.2196, 0.2314, 0.2627)), \
+           mlab.triangular_mesh(cockpit5_v_x, cockpit5_v_y, cockpit5_v_z, cockpit5_f, color=(0.2353, 0.2588, 0.2941)), \
+           mlab.triangular_mesh(cockpit6_v_x, cockpit6_v_y, cockpit6_v_z, cockpit6_f, color=(0.1686, 0.1765, 0.1961))
 
 
 def process_obj_file_v(original: str, new_file: str, type: str, obj_name: str):
@@ -124,9 +130,9 @@ def process_obj_file_v(original: str, new_file: str, type: str, obj_name: str):
             split = line.split()
             if split[0] == 'v':
                 new_line_counter += 1
-                out_x.write(f'{float(split[1])}, ')
-                out_y.write(f'{float(split[2])}, ')
-                out_z.write(f'{float(split[3])}, ')
+                out_x.write(f'{float(split[1])/55}, ')
+                out_y.write(f'{float(split[2])/55}, ')
+                out_z.write(f'{float(split[3])/55}, ')
                 if new_line_counter % 5 == 0:
                     out_x.write('\n     ')
                     out_y.write('\n     ')
@@ -164,14 +170,14 @@ def process_obj_file_f(original: str, new_file: str, type: str, obj_name: str):
 # process_obj_file_v('data/obj/other_buildings.obj', 'data/vertices/other_buildings_v', '.py', 'other_buildings_v')
 # process_obj_file_v('data/obj/main_building.obj', 'data/vertices/main_building_v', '.py', 'main_building_v')
 # process_obj_file_v('data/obj/water.obj', 'data/vertices/water_v', '.py', 'water_v')
-# process_obj_file_v('data/obj/cockpit_base.obj', 'data/vertices/cockpit_base_v', '.py', 'cockpit_base_v')
-# process_obj_file_v('data/obj/cockpit0.obj', 'data/vertices/cockpit0_v', '.py', 'cockpit0_v')
-# process_obj_file_v('data/obj/cockpit1.obj', 'data/vertices/cockpit1_v', '.py', 'cockpit1_v')
-# process_obj_file_v('data/obj/cockpit2.obj', 'data/vertices/cockpit2_v', '.py', 'cockpit2_v')
-# process_obj_file_v('data/obj/cockpit3.obj', 'data/vertices/cockpit3_v', '.py', 'cockpit3_v')
-# process_obj_file_v('data/obj/cockpit4.obj', 'data/vertices/cockpit4_v', '.py', 'cockpit4_v')
-# process_obj_file_v('data/obj/cockpit5.obj', 'data/vertices/cockpit5_v', '.py', 'cockpit5_v')
-# process_obj_file_v('data/obj/cockpit6.obj', 'data/vertices/cockpit6_v', '.py', 'cockpit6_v')
+#     process_obj_file_v('data/obj/cockpit_base.obj', 'data/vertices/cockpit_base_v', '.py', 'cockpit_base_v')
+#     process_obj_file_v('data/obj/cockpit0.obj', 'data/vertices/cockpit0_v', '.py', 'cockpit0_v')
+#     process_obj_file_v('data/obj/cockpit1.obj', 'data/vertices/cockpit1_v', '.py', 'cockpit1_v')
+#     process_obj_file_v('data/obj/cockpit2.obj', 'data/vertices/cockpit2_v', '.py', 'cockpit2_v')
+#     process_obj_file_v('data/obj/cockpit3.obj', 'data/vertices/cockpit3_v', '.py', 'cockpit3_v')
+#     process_obj_file_v('data/obj/cockpit4.obj', 'data/vertices/cockpit4_v', '.py', 'cockpit4_v')
+#     process_obj_file_v('data/obj/cockpit5.obj', 'data/vertices/cockpit5_v', '.py', 'cockpit5_v')
+#     process_obj_file_v('data/obj/cockpit6.obj', 'data/vertices/cockpit6_v', '.py', 'cockpit6_v')
 
 # process_obj_file_f('data/obj/wind_rose.obj', 'data/faces/wind_rose_f', '.py', 'wind_rose_f')
 # process_obj_file_f('data/obj/terrain_detailed.obj', 'data/faces/terrain_detailed_f', '.py', 'terrain_detailed_f')
@@ -179,14 +185,14 @@ def process_obj_file_f(original: str, new_file: str, type: str, obj_name: str):
 # process_obj_file_f('data/obj/other_buildings.obj', 'data/faces/other_buildings_f', '.py', 'other_buildings_f')
 # process_obj_file_f('data/obj/main_building.obj', 'data/faces/main_building_f', '.py', 'main_building_f')
 # process_obj_file_f('data/obj/water.obj', 'data/faces/water_f', '.py', 'water_f')
-# process_obj_file_f('data/obj/cockpit_base.obj', 'data/faces/cockpit_base_f', '.py', 'cockpit_base_f')
-# process_obj_file_f('data/obj/cockpit0.obj', 'data/faces/cockpit0_f', '.py', 'cockpit0_f')
-# process_obj_file_f('data/obj/cockpit1.obj', 'data/faces/cockpit1_f', '.py', 'cockpit1_f')
-# process_obj_file_f('data/obj/cockpit2.obj', 'data/faces/cockpit2_f', '.py', 'cockpit2_f')
-# process_obj_file_f('data/obj/cockpit3.obj', 'data/faces/cockpit3_f', '.py', 'cockpit3_f')
-# process_obj_file_f('data/obj/cockpit4.obj', 'data/faces/cockpit4_f', '.py', 'cockpit4_f')
-# process_obj_file_f('data/obj/cockpit5.obj', 'data/faces/cockpit5_f', '.py', 'cockpit5_f')
-# process_obj_file_f('data/obj/cockpit6.obj', 'data/faces/cockpit6_f', '.py', 'cockpit6_f')
-
-
+#     process_obj_file_f('data/obj/cockpit_base.obj', 'data/faces/cockpit_base_f', '.py', 'cockpit_base_f')
+#     process_obj_file_f('data/obj/cockpit0.obj', 'data/faces/cockpit0_f', '.py', 'cockpit0_f')
+#     process_obj_file_f('data/obj/cockpit1.obj', 'data/faces/cockpit1_f', '.py', 'cockpit1_f')
+#     process_obj_file_f('data/obj/cockpit2.obj', 'data/faces/cockpit2_f', '.py', 'cockpit2_f')
+#     process_obj_file_f('data/obj/cockpit3.obj', 'data/faces/cockpit3_f', '.py', 'cockpit3_f')
+#     process_obj_file_f('data/obj/cockpit4.obj', 'data/faces/cockpit4_f', '.py', 'cockpit4_f')
+#     process_obj_file_f('data/obj/cockpit5.obj', 'data/faces/cockpit5_f', '.py', 'cockpit5_f')
+#     process_obj_file_f('data/obj/cockpit6.obj', 'data/faces/cockpit6_f', '.py', 'cockpit6_f')
+#
+#
 # main()
--- main.py
+++ main.py
@@ -5,14 +5,36 @@ from typing import Tuple
 from helper import create_wind_rose, create_main_building, create_other_buildings, create_terrain_large, \
     create_terrain_detailed, create_line, create_water, create_cockpit
 
-dt = .016
+Custom = (2426, 1365)
+mlab.figure(bgcolor=(0.1255, 0.5176, 0.8157), size=Custom)
+
+dt = 0.016
 delay = int(dt * 1000)
 
+water = create_water()
+line, i_y, intensity = create_line()
+wind_rose = create_wind_rose()
+main_building = create_main_building()
+other_buildings = create_other_buildings()
+terrain_large = create_terrain_large()
+terrain_detailed = create_terrain_detailed()
+use_cockpit = False
+if use_cockpit:
+    cockpit = create_cockpit()
+    for obj in cockpit:
+        obj.actor.actor.rotate_z(180)
+        obj.actor.actor.rotate_x(90)
+else:
+    cockpit = None
+
+
+# scene = mlab.get_engine().scenes[0]
+# scene.scene.movie_maker.record = True
 
 def get_intensity(index: int, d: float) -> Tuple[float, float, int]:
     current_intensity = intensity[index]
-    factor = 70
-    right = .01 * np.sin(d * 2) * current_intensity * 0.01 * factor
+    factor = 60
+    right = 0.001 * np.sin(d * 2) * current_intensity * 0.01 * factor
     up = -current_intensity * 0.01 * factor
     up += 4.86 * 0.01 * factor  # To stay at center of tube (avoid sudden drop when tube is entered)
 
@@ -22,18 +44,8 @@ def get_intensity(index: int, d: float) -> Tuple[float, float, int]:
 
     return right, up, index
 
+    # TODO change origin
 
-# water = create_water()
-line, i_y, intensity = create_line()
-# wind_rose = create_wind_rose()
-# main_building = create_main_building()
-# other_buildings = create_other_buildings()
-# terrain_large = create_terrain_large()
-# terrain_detailed = create_terrain_detailed()
-cockpit = create_cockpit()
-for obj in cockpit:
-    obj.actor.actor.rotate_z(180)
-    obj.actor.actor.rotate_x(90)
 
 @mlab.animate(delay=delay)
 def anim():
@@ -41,9 +53,10 @@ def anim():
     line_flatten = (322.5, -302.7, 18.0)
     line_end = (322.5, -518.0, 18.0)
     cam_start = (322.5, 1000, line_start[2] + np.tan(np.deg2rad(3)) * (1000 - line_start[1]))
-    # TODO scale the cockpit and color change
-    for obj in cockpit:
-        obj.actor.actor.position = (cam_start[0], cam_start[1]-2, cam_start[2])
+
+    if use_cockpit:
+        for obj in cockpit:
+            obj.actor.actor.position = (cam_start[0], cam_start[1] - 6, cam_start[2]-0.8)
     mlab.view(azimuth=90, elevation=87, distance=.1, focalpoint=cam_start)
 
     current_fig = mlab.gcf()
@@ -54,35 +67,64 @@ def anim():
     v_dt = v * dt
     t = 0
     d = 0
+    plane_angle = 3
     index = len(intensity) - 1
     prev_i_move = (0, 0)
+    add_y = True
 
     pitch_start = line_flatten[1] - 10
     pitch_end = line_flatten[1] + 10
-    pitch_delta = 3. / ((pitch_end - pitch_start) / (v * dt))
+    pitch_delta = 3 / ((pitch_end - pitch_start) / v_dt)
+
 
     while True:
-        # if t > 3.:
-        #     if cam.position[1] <= line_start[1]:
-        #         mlab.move(right=-prev_i_move[0], up=-prev_i_move[1])
-        #         mlab.move(v_dt)
-        #         right, up, index = get_intensity(index, d)
-        #         mlab.move(right=right, up=up)
-        #         prev_i_move = (right, up)
-        #         d += v_dt
-        #     else:
-        #         mlab.move(v_dt)
-        #
-        #     if pitch_start < cam.position[1] < pitch_end:
-        #         cam.pitch(pitch_delta)
-        #
-        #     if cam.position[1] <= line_end[1]:
-        #         print(d)
-        #         break
-
+    #     if t > 3.:
+    #         if cam.position[1] <= line_start[1]:
+    #             mlab.move(right=-prev_i_move[0], up=-prev_i_move[1])
+    #             if use_cockpit:
+    #                 pos1 = cockpit[0].actor.actor.position
+    #                 for obj in cockpit:
+    #                     obj.actor.actor.position = (pos1[0] - prev_i_move[0], pos1[1], pos1[2] - prev_i_move[1])
+    #                     # obj.actor.actor.position = (pos1[0], pos1[1], pos1[2])
+    #
+    #             right, up, index = get_intensity(index, d)
+    #             mlab.move(v_dt)
+    #             mlab.move(right=right, up=up)
+    #
+    #             if use_cockpit:
+    #                 for obj in cockpit:
+    #                     pos2 = obj.actor.actor.position
+    #                     obj.actor.actor.position = (pos2[0]+right, pos2[1]-v_dt + (.0022 if add_y else 0.), pos2[2]-(np.tan(np.deg2rad(plane_angle)) * v_dt)+up)
+    #                     # obj.actor.actor.position = (pos2[0], pos2[1] - v_dt + (.0022 if add_y else 0.), pos2[2] - np.tan(np.deg2rad(plane_angle)) * v_dt)
+    #
+    #             prev_i_move = (right, up)
+    #             d += v_dt
+    #
+    #         else:
+    #             mlab.move(v_dt)
+    #             if use_cockpit:
+    #                 for obj in cockpit:
+    #                     p = obj.actor.actor.position
+    #                     obj.actor.actor.position = (p[0], p[1] - v_dt + .0022, p[2] - np.tan(np.deg2rad(plane_angle)) * v_dt)
+    #
+    #
+    #
+    #         if pitch_start < cam.position[1] < pitch_end:
+    #             add_y = False
+    #             cam.pitch(pitch_delta)
+    #             mlab.move(up=-.012)
+    #             if use_cockpit:
+    #                 for obj in cockpit:
+    #                     obj.actor.actor.rotate_x(-pitch_delta)
+    #             plane_angle -= pitch_delta
+    #
+    #         if cam.position[1] <= line_end[1]:
+    #             break
+    #
         scene.render()
         t += dt
         yield
+
 
 
 anim()
